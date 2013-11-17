d6ae10be2333329207b2b3b9cac54bcc1ad0bbbb
anakpisces@gmail.com <anakpisces@gmail.com@057ba487-a534-6db7-afb7-6fedc991237d>
2013-11-17 06:15:50 +0000

    remove for new

--- trunk/application/logs/log-2013-09-12.php
+++
@@ -1,4 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2013-09-12 22:06:51 --> Error : No XHR access : admin
-ERROR - 2013-09-12 22:06:53 --> Error : No XHR access : admin
--- trunk/modules/Demo/views/alert.php
+++
@@ -1,6 +0,0 @@
-<div class="alert" >
-<button type="button" class="close" data-dismiss="alert">×</button>
-<h4>
-Alert!
-</h4> <strong>Warning!</strong> For security reason, please sign out when leave your work.
-</div>
